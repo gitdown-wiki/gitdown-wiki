61249c1ba1ebabe1c28f31e7a76fbd9c99a0b880
Marius Büscher <marius.buescher@gmx.de>
2016-01-02 09:55:12 +0100

    Add repository service

---
+++ src/AppBundle/Service/RepositoryService.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace AppBundle\Service;
+
+use \Gitonomy\Git\Repository;
+
+class RepositoryService
+{
+    protected $rootPath;
+    
+    function __construct($rootPath)
+    {
+        $this->rootPath = $rootPath;
+    }
+    
+    public function getRepository($repositoryPath)
+    {
+        $path = realpath(__DIR__ . '../../../' . $this->rootPath . '/' . $repositoryPath);
+        $repository = new Repository($path);
+        
+        return $repository;
+    }
+}
---
+++ src/AppBundle/Tests/Service/RepositoryServiceTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace AppBundle\Tests\Service;
+
+use \AppBundle\Service\RepositoryService;
+use \Gitonomy\Git\Repository;
+use Symfony\Component\Filesystem\Filesystem;
+
+class RepositoryServiceTest extends \PHPUnit_Framework_TestCase
+{
+    public function tearDown()
+    {
+        $repoName = 'test';
+        $rootPath = 'app/data';
+        
+        $fs = new Filesystem();
+        
+        $fs->remove(realpath(__DIR__ . '/../../../../' . $rootPath . '/' . $repoName));
+    }
+    
+    public function testRepositoryCreation()
+    {
+        $repoName = 'test';
+        $rootPath = 'app/data';
+        
+        $repoPath = __DIR__ . '/../../../../' . $rootPath . '/' . $repoName;
+        
+        $fs = new Filesystem();
+        $fs->mkdir($repoPath);
+        
+        $repo = \Gitonomy\Git\Admin::init($repoPath, false);
+        
+        $repoService = new RepositoryService($rootPath);
+        $testRepo = $repoService->getRepository($repoName);
+        
+        $this->assertInstanceOf('\Gitonomy\Git\Repository', $testRepo);
+    }
+}
