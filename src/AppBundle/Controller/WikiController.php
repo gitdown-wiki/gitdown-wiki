ab9c8fb48b0901cc006a253913f281ad982054ab
Marius Büscher <marius.buescher@gmx.de>
2016-01-03 10:13:35 +0100

    Add basic wiki controller

---
+++ src/AppBundle/Controller/WikiController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity\Wiki;
+
+class WikiController extends Controller
+{
+    /**
+     * @Route("/wiki/new", name="wiki_new")
+     * @Method("GET")
+     */
+    public function newAction()
+    {
+        return $this->render('wiki/new.html.twig', array());
+    }
+    
+    /**
+     * @Route("/wiki/new", name="wiki_create")
+     * @Method("POST")
+     */
+    public function createAction(Request $request)
+    {
+        $wiki = new Wiki();
+        
+        $name = $request->request->get('name');
+        $wiki->setName($name);
+        
+        $slug = $request->request->get('slug');
+        if (empty($slug)) {
+            $slug = $this->get('slug')->slugify($name);
+        }
+        $wiki->setSlug($slug);
+        
+        $em = $this->getDoctrine()->getManager();
+        
+        $em->persist($wiki);
+        $em->flush();
+        
+        $repositoryService = $this->get('app.repository');
+        
+        $repository = $repositoryService->createRepository($slug);
+        
+        $path = $repository->getWorkingDir();
+        
+        $fs = new Filesystem();
+        $fs->dumpFile($path . '/index.md', '# ' . $name);
+        
+        $repository->run('add', array('-A'));
+        $repository->run('commit', array('-m "Initial commit"', '--author="Gitdown wiki <wiki@example.com>"'));
+        
+        return $this->redirectToRoute('wiki_showpage', array('slug' => $slug));
+    }
+    
+    /**
+     * @Route("/wiki", name="wiki_index")
+     * @Method("GET")
+     */
+    public function indexAction()
+    {
+        $wikiRepository = $this->getDoctrine()->getRepository('AppBundle:Wiki');
+        
+        $wikis = $wikiRepository->findAll();
+        
+        return $this->render('wiki/index.html.twig', array(
+            'wikis' => $wikis
+        ));
+    }
+    
+    /**
+     * @Route("/wiki/{slug}/{page}", name="wiki_showpage", requirements={
+     *     "page": "[\d\w-_\/\.+@*]+"
+     * }, defaults={
+     *     "page": "index"
+     * }))
+     * @Method("GET")
+     */
+    public function showPageAction($slug, $page)
+    {
+        $wikiRepository = $this->getDoctrine()->getRepository('AppBundle:Wiki');
+        $wiki = $wikiRepository->findOneBySlug($slug);
+        
+        $repository = $this->get('app.repository')->getRepository($slug);
+        $branch = $repository->getReferences()->getBranch('master');
+        $commit = $branch->getCommit();
+        $tree = $commit->getTree();
+        $blob = $tree->resolvePath($page . '.md');
+        
+        return $this->render('wiki/showPage.html.twig', array(
+            'wiki' => $wiki,
+            'tree' => $tree->getEntries(),
+            'content' => $blob->getContent()
+        ));
+    }
+}
--- src/AppBundle/Twig/AppExtension.php
+++ src/AppBundle/Twig/AppExtension.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Twig;
 
 use AppBundle\Service\MarkdownService;
+use Gitonomy\Git\Tree;
 
 class AppExtension extends \Twig_Extension
 {
@@ -22,12 +23,42 @@ class AppExtension extends \Twig_Extension
                 array($this, 'markdownToHtml'),
                 array('is_safe' => array('html'))
             ),
+            new \Twig_SimpleFilter(
+                'isTree',
+                array($this, 'checkIfTree')
+            ),
+            new \Twig_SimpleFilter(
+                'pageFromMarkdown',
+                array($this, 'generatePageFromMarkdown')
+            ),
+            new \Twig_SimpleFilter(
+                'extendPagePath',
+                array($this, 'extendPagePath')
+            )
         );
     }
     
     public function markdownToHtml($content)
     {
         return $this->parser->toHtml($content);
+    }
+    
+    public function checkIfTree($object)
+    {
+        return $object instanceof Tree;
+    }
+    
+    public function generatePageFromMarkdown($markdown, $basePath)
+    {
+        $path = (strlen($basePath) > 0) ? $basePath . '/' : '';
+        return  $path . str_replace('.md', '', $markdown);
+    }
+    
+    public function extendPagePath($basePath, $extension)
+    {
+        $path = $basePath;
+        $path .= (strlen($basePath) > 0) ? '/' : '';
+        return $path . $extension;
     }
     
     public function getName()
